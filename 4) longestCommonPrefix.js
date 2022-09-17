a875050c0121666325998523dca23b8c8b15980f
Tom Padmanathan <padmanathantom@gmail.com>
2022-09-17 15:58:07 +0100

    Completed code for longestCommonPrefix.js

---
+++ 4) longestCommonPrefix.js
@@ -0,0 +1,14 @@
+let input = ['flower', 'flow', 'flight']
+let prefixArr = []
+let output = ''
+
+for(let i = 0; i < input.length; i++) {
+    prefixArr.push(input[i].split('')[0] + input[i].split('')[1])
+}
+if(prefixArr[0] == prefixArr[1] && prefixArr[0] == prefixArr[2]) {
+    output = prefixArr[0]
+} else {
+    output = 'There is no common prefix among the input strings.'
+}
+
+console.log(output)
