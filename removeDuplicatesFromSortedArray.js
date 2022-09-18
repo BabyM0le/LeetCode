6287cd5d2f1310f448676b23509e324ead3a7a16
Tom Padmanathan <padmanathantom@gmail.com>
2022-09-18 20:23:57 +0100

    t

--- removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray.js
@@ -1,7 +1,14 @@
 let input = [1, 1, 2]
-let output
+let output = input
 
 for(let i = 0; i < input.length; i++) {
+    console.log(`I = ${i}`)
+    for(let u = 1; u < input.length; u++) {
+        console.log(`U = ${u}`)
+        if(input[i] == input[u]) {
+            output[i] = ''
+        }
+    }
 }
 
 console.log(output)
